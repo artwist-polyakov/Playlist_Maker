ffc9971719c638da552ee9852c658d0530f6195a
Aleksandr Polyakov <master@artwist.ru>
2023-04-03 21:28:16 +0300

    Способ 3. Реализация OnClickListener на Activity

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -6,15 +6,19 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val image = findViewById<ImageView>(R.id.poster)
+        image.setOnClickListener(this@MainActivity)
+    }
 
-        image.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show()
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.poster -> {
+                Toast.makeText(this, "Нажали на картинку!", Toast.LENGTH_SHORT).show()
+            }
         }
-
     }
 }
