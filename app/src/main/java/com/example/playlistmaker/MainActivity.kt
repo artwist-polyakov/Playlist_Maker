10248637c02c502441d4a2ca49560610f3dcd196
Aleksandr Polyakov <master@artwist.ru>
2023-04-03 21:18:50 +0300

    Ничего не происходит — только глухой звук

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
-
+//  не работает
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
