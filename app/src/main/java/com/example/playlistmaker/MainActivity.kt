143868ecca8ca87968196535e935c2e70acfe558
Aleksandr Polyakov <master@artwist.ru>
2023-04-03 21:21:50 +0300

    Ничего не происходит — только глухой звук

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -11,12 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val image = findViewById<ImageView>(R.id.poster)
-        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Здесь какой-то текст", Toast.LENGTH_SHORT)
-            }
+
+        image.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show()
         }
-        image.setOnClickListener(imageClickListener)
 
     }
 }
