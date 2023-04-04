8b282235d20cc53d875c7e89485396af18c29362
Aleksandr Polyakov <master@artwist.ru>
2023-04-04 22:13:54 +0300

    Приложение вылетает при попытке открытия экрана настроек

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -18,22 +19,34 @@ class MainActivity : AppCompatActivity() {
 
         //через лямбду
         searchButton.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Нажали на кнопку ПОИСК 🔎 ", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this@MainActivity, "Нажали на кнопку ПОИСК 🔎 ", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@MainActivity, SearchActivity::class.java)
+            startActivity(intent)
         }
 
-        //через анонимный класс
-        val mediaButtonClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Нажали на кнопку МЕДИА 🎶", Toast.LENGTH_SHORT)
-                    .show()
-            }
+        mediaButton.setOnClickListener {
+//            Toast.makeText(this@MainActivity, "Нажали на кнопку ПОИСК 🔎 ", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@MainActivity, MediaActivity::class.java)
+            startActivity(intent)
         }
-        mediaButton.setOnClickListener(mediaButtonClickListener)
+
+//        //через анонимный класс
+//        val mediaButtonClickListener: View.OnClickListener = object : View.OnClickListener {
+//            override fun onClick(v: View?) {
+////                Toast.makeText(this@MainActivity, "Нажали на кнопку МЕДИА 🎶", Toast.LENGTH_SHORT)
+////                    .show()
+//                val intent = Intent(this@MainActivity, MediaActivity::class.java)
+//                startActivity(intent)
+//            }
+//        }
+//        mediaButton.setOnClickListener(mediaButtonClickListener)
 
 
         //через лямбду
         settingsButton.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Нажали на кнопку НАСТРОЙКИ ⚙️ ", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this@MainActivity, "Нажали на кнопку НАСТРОЙКИ ⚙️ ", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@MainActivity, SettingsActivity::class.java)
+            startActivity(intent)
         }
     }
 }
