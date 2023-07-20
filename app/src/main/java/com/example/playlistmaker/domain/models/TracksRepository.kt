b0b97a6af2397a0eb944a9dfe9bc510f14fe35e2
Aleksandr Poliakov <master@artwist.ru>
2023-07-20 23:28:48 +0300

    Спринт 15.
    Переопределил в линкед репозитории класс в Dto (потому что в конце мы переименуем TrackDto и Track и поменяем их местами

--- app/src/main/java/com/example/playlistmaker/domain/models/TracksRepository.kt
+++ app/src/main/java/com/example/playlistmaker/domain/models/TracksRepository.kt
@@ -1,10 +1,11 @@
 package com.example.playlistmaker.domain.models
 
 import android.content.Context
+import com.example.playlistmaker.data.dto.TrackDto
 
 interface TracksRepository {
-    fun add(item: Track)
-    fun get(reverse: Boolean): ArrayList<Track>?
+    fun add(item: TrackDto)
+    fun get(reverse: Boolean): ArrayList<TrackDto>?
 
     fun restoreFromSharedPreferences(prefs_name: String, key: String, context: Context)
 
