4faa342128146e99d7264ecf3c7ed0341bfa5e3d
Aleksandr Poliakov <master@artwist.ru>
2023-08-14 22:14:17 +0300

    Спринт 17: KOIN Попробовал инъекцию зависимостей в класс App

--- app/src/main/java/com/example/playlistmaker/common/App.kt
+++ app/src/main/java/com/example/playlistmaker/common/App.kt
@@ -1,28 +1,27 @@
-package com.example.playlistmaker
+package com.example.playlistmaker.common
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import com.example.playlistmaker.common.data.ThemeSettings
-import com.example.playlistmaker.creator.Creator
+import com.example.playlistmaker.common.di.repositoryModule
+import com.example.playlistmaker.settings.data.SettingsRepository
+import org.koin.android.ext.android.inject
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
-class App : Application() {
+class App  : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        when (Creator.provideCurrentTheme(this)) {
+        startKoin {
+            androidContext(this@App)
+            modules(repositoryModule)
+        }
+        val settings: SettingsRepository by inject()
+        when (settings.getThemeSettings()) {
             ThemeSettings.Dark -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
             ThemeSettings.Light -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             else -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM) // или другой режим по умолчанию
         }
-    }
-
-    fun switchTheme(darkThemeEnabled: Boolean) {
-        AppCompatDelegate.setDefaultNightMode(
-            if (darkThemeEnabled) {
-                AppCompatDelegate.MODE_NIGHT_YES
-            } else {
-                AppCompatDelegate.MODE_NIGHT_NO
-            }
-        )
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/common/di/RepositoryModule.kt
@@ -0,0 +1,13 @@
+package com.example.playlistmaker.common.di
+
+import com.example.playlistmaker.common.data.ThemeRepositoryImpl
+import com.example.playlistmaker.common.domain.api.ThemeRepository
+import com.example.playlistmaker.settings.data.SettingsRepository
+import com.example.playlistmaker.settings.data.SettingsRepositoryImpl
+import org.koin.dsl.module
+
+val repositoryModule = module {
+    single<ThemeRepository> { ThemeRepositoryImpl(get()) }
+
+    single<SettingsRepository> { SettingsRepositoryImpl(get()) }
+}
