207e7d7c71d457471b17fb147c0b717f79c5bd38
Aleksandr Poliakov <master@artwist.ru>
2023-08-16 19:24:25 +0300

    Спринт 17: KOIN Минус метод в креаторе

--- app/src/main/java/com/example/playlistmaker/creator/Creator.kt
+++ app/src/main/java/com/example/playlistmaker/creator/Creator.kt
@@ -3,7 +3,6 @@ package com.example.playlistmaker.creator
 import android.content.Context
 import com.example.playlistmaker.common.domain.api.ThemeRepository
 import com.example.playlistmaker.common.data.ThemeRepositoryImpl
-import com.example.playlistmaker.settings.domain.ThemeUseCase
 import com.example.playlistmaker.search.data.network.RetrofitNetworkClient
 import com.example.playlistmaker.search.data.network.TracksRepositoryImpl
 import com.example.playlistmaker.search.data.storage.TracksStorageImpl
@@ -12,7 +11,6 @@ import com.example.playlistmaker.search.domain.impl.TracksInteractorImpl
 import com.example.playlistmaker.settings.data.ExternalNavigatorImpl
 import com.example.playlistmaker.settings.data.SettingsRepository
 import com.example.playlistmaker.settings.data.SettingsRepositoryImpl
-import com.example.playlistmaker.settings.domain.ThemeUseCaseImpl
 import com.example.playlistmaker.settings.domain.ExternalNavigator
 import com.example.playlistmaker.settings.domain.NavigationInteractor
 import com.example.playlistmaker.settings.domain.SettingsInteractor
@@ -33,10 +31,6 @@ object Creator {
 
     private fun getThemeRepository(context: Context): ThemeRepository {
         return ThemeRepositoryImpl(context)
-    }
-
-    fun provideThemeUseCase(context: Context): ThemeUseCase {
-        return ThemeUseCaseImpl(getThemeRepository(context))
     }
 
     private fun getSettingsRepository(context: Context): SettingsRepository {
