05562aa814fd73a336ccbc8beb967c1dd9a3de71
Aleksandr Poliakov <master@artwist.ru>
2023-09-02 22:00:26 +0300

    Спринт 18: FRAGMENTS Не получается убрать капс у таблайаут

--- app/src/main/java/com/example/playlistmaker/media/ui/activity/MediaViewPagerAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/media/ui/activity/MediaViewPagerAdapter.kt
@@ -7,7 +7,10 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 class MediaViewPagerAdapter (fragmentManager: FragmentManager, lifecycle: Lifecycle)
     : FragmentStateAdapter(fragmentManager, lifecycle) {
 
-    val fragments = listOf(PlaylistsFragment.newInstance(), FavoritesFragment.newInstance())
+    val fragments = listOf(
+        FavoritesFragment.newInstance(),
+        PlaylistsFragment.newInstance()
+    )
 
     override fun getItemCount() = fragments.size
 
