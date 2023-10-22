fb8738fc7be475c8c58e007173adc41d3bf42905
Aleksandr Poliakov <master@artwist.ru>
2023-10-22 21:50:37 +0300

    Созданы классы вьюмодели и фрагмента для создания плейлиста

---
+++ app/src/main/java/com/example/playlistmaker/media/ui/fragments/CreatePlaylistFragment.kt
@@ -0,0 +1,6 @@
+package com.example.playlistmaker.media.ui.fragments
+
+import androidx.fragment.app.Fragment
+
+class CreatePlaylistFragment: Fragment() {
+}
---
+++ app/src/main/java/com/example/playlistmaker/media/ui/view_model/CreatePlaylistViewmodel.kt
@@ -0,0 +1,6 @@
+package com.example.playlistmaker.media.ui.view_model
+
+import androidx.lifecycle.ViewModel
+
+class CreatePlaylistViewmodel: ViewModel() {
+}
