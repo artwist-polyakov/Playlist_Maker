4bdbf0d7f4f370cdecbc70454b5e4ede9db5447c
Aleksandr Poliakov <master@artwist.ru>
2023-10-27 14:27:23 +0300

    Дата классы для экрана с плейлистами

---
+++ app/src/main/java/com/example/playlistmaker/media/ui/view_model/states/PlaylistsScreenState.kt
@@ -0,0 +1,8 @@
+package com.example.playlistmaker.media.ui.view_model.states
+
+import com.example.playlistmaker.common.presentation.models.PlaylistInformation
+
+sealed class PlaylistsScreenState {
+    data class Content(val content: List<PlaylistInformation>): PlaylistsScreenState()
+    object Empty: PlaylistsScreenState()
+}
