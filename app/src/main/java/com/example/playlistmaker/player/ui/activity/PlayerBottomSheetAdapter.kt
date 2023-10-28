1c4bf6625475fa692ecb76c341589aa43634abe7
Aleksandr Poliakov <master@artwist.ru>
2023-10-28 16:43:24 +0300

    Добавил адаптер и вьюхолдер для ресайклера

---
+++ app/src/main/java/com/example/playlistmaker/player/ui/activity/PlayerBottomSheetAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.playlistmaker.player.ui.activity
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.playlistmaker.common.presentation.models.PlaylistInformation
+
+
+class PlayerBottomSheetAdapter(private val clickListener: PlayerBottomSheetAdapter.PlaylistClickListener) : RecyclerView.Adapter<PlayerBottomSheetViewHolder>() {
+    var playlists = ArrayList<PlaylistInformation>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlayerBottomSheetViewHolder = PlayerBottomSheetViewHolder(parent, clickListener)
+    override fun onBindViewHolder(holder: PlayerBottomSheetViewHolder, position: Int) {
+        val playlist = playlists.get(position)
+        holder.bind(playlist)
+        holder.itemView.setOnClickListener {
+            clickListener.onTrackClick(playlist)
+        }
+    }
+    override fun getItemCount(): Int = playlists.size
+
+    fun updatePlaylists(newPlaylists: List<PlaylistInformation>) {
+        playlists.clear()
+        playlists.addAll(newPlaylists)
+        notifyDataSetChanged()
+    }
+
+    interface PlaylistClickListener {
+        fun onTrackClick(playlist: PlaylistInformation)
+    }
+}
