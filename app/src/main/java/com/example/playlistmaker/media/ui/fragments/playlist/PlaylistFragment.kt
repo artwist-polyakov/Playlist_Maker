dea356d4d86fe9a80e6b281ab5de8c0f561cd329
Aleksandr Poliakov <master@artwist.ru>
2023-11-02 19:10:08 +0300

    Созданы плейсхолдеры для олтдельных плейлистов

---
+++ app/src/main/java/com/example/playlistmaker/media/ui/fragments/playlist/PlaylistFragment.kt
@@ -0,0 +1,27 @@
+package com.example.playlistmaker.media.ui.fragments.playlist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.playlistmaker.R
+
+class PlaylistFragment: Fragment() {
+
+    override fun onCreateView (
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_playlist, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        fun newInstance() = PlaylistFragment()
+    }
+}
