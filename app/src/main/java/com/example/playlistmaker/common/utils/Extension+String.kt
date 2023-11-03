69cf7a4c64de18aa2ad1c6c5e781162298e2fb19
Aleksandr Poliakov <master@artwist.ru>
2023-11-03 22:18:40 +0300

    Котлин-стиль

--- app/src/main/java/com/example/playlistmaker/common/utils/Extension+String.kt
+++ app/src/main/java/com/example/playlistmaker/common/utils/Extension+String.kt
@@ -1,11 +1,8 @@
 package com.example.playlistmaker.common.utils
 
 
-fun String.countDurationInSeconds(): Long {
-    val timeUnits = listOf(1, 60, 3600)
-    var result = 0L
+fun String.countDurationInSeconds(): Long =
     this.split(":").reversed().mapIndexed { index, timeComponent ->
-        result += (timeComponent.toIntOrNull() ?: 0) * (timeUnits.getOrElse(index) { 1 })
-    }
-    return result
-}
+        timeComponent.toInt() * (listOf(1, 60, 3600).getOrElse(index) { 0 })
+    }.sum().toLong()
+
