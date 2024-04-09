33a07dff4beab5bd94d671b5bc83dc2a7254f74a
Aleksandr Poliakov <master@artwist.ru>
2024-04-09 21:04:18 +0300

    описывает логику сервиса, чтобы тот выводил логи

--- app/src/main/java/com/example/playlistmaker/player/service/PlaylistMakerMusicService.kt
+++ app/src/main/java/com/example/playlistmaker/player/service/PlaylistMakerMusicService.kt
@@ -3,11 +3,29 @@ package com.example.playlistmaker.player.service
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
 
 internal class PlaylistMakerMusicService: Service() {
     override fun onBind(intent: Intent?): IBinder? {
         TODO("Not yet implemented")
         return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d(LOG_TAG, "Service created")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d(LOG_TAG, "onStartCommand | flags: $flags, startId: $startId")
+        val url = intent?.getStringExtra(EXTRA_URL_TAG)
+        Log.d(LOG_TAG, "onStartCommand | url: $url")
+        return Service.START_NOT_STICKY
+    }
+
+    companion object   {
+        const val EXTRA_URL_TAG = "trackUrl"
+        const val LOG_TAG = "PlaylistMakerMusicService"
     }
 
 }
