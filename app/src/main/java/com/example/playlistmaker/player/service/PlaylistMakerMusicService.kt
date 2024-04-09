710949f40c2bd9741c911db413de447419613ec7
Aleksandr Poliakov <master@artwist.ru>
2024-04-09 19:28:21 +0300

    Создаёт класс для бущушего сервиса
    Размещает новый класс в пакет Service приложения

---
+++ app/src/main/java/com/example/playlistmaker/player/service/PlaylistMakerMusicService.kt
@@ -0,0 +1,13 @@
+package com.example.playlistmaker.player.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+internal class PlaylistMakerMusicService: Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+        return null
+    }
+
+}
